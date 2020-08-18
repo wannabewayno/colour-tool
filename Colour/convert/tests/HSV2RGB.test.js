da7a42127d220aec07814104b06e25bb50eaa7aa
Wayne <wayne.c.griffiths@gmail.com>
2020-08-18 18:50:28 +0800

    ADD updated colours.js to include hsv conversions also

--- Colour/convert/tests/HSV2RGB.test.js
+++ Colour/convert/tests/HSV2RGB.test.js
@@ -1,7 +1,16 @@
 const HSV2RGB = require('../HSV2RGB');
+const Colour = require('../../Colour');
+const colours = require('./colours');
 
-describe("HSV2RGB", () => {
-    
+describe("HSV2RGB()", () => {
+
     // Positive test
-    it("Should convert ")
-}
+    colours.forEach(colour => {
+        it(`should convert ${colour.hsv} to ${colour.rgb}` , () => {
+            const convertFrom = new Colour(colour.hsv).getChannels();
+            const convertTo = new Colour(colour.rgb).getChannels();
+            expect(HSV2RGB(...convertFrom)).toStrictEqual(convertTo);
+        });
+    });
+        
+});
