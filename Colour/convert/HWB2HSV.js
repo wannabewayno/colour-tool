944dfcb7041ad24d9fd888553aea99e9bc99ef46
Wayne <wayne.c.griffiths@gmail.com>
2020-08-18 23:12:07 +0800

    ADD HWB2HSV.js and it's test HWB2HSV.test.js, also added boilerplate code from the w3 CSS4 specifications for the CIELAB and CIELCH colour spaces for later integration

---
+++ Colour/convert/HWB2HSV.js
@@ -0,0 +1,25 @@
+require('../mathExtension')();
+
+/**
+ * converts a HSV colour into a HWB colour
+ * @param {Number} H - Hue [0,360] 
+ * @param {Number} W - Whiteness [0,100] 
+ * @param {Number} B - Blackness [0,100] 
+ */
+module.exports = (H,W,B) => {
+    W /= 100 // change range from [0,100] to  [0,1]
+    B /= 100 // change range from [0,100] to  [0,1] 
+
+    const V = 1 - B;
+
+    // Exception if V is 0, All black no saturation.
+    if (V === 0) return [H,0,V];
+
+    const S = 1 - W/V;
+    
+    return [
+        H,
+        Math.decimal( S * 100, 1),
+        Math.decimal( V * 100, 1)
+    ];
+}
---
+++ Colour/convert/tests/HWB2HSV.test.js
@@ -0,0 +1,15 @@
+const HWB2HSV = require('../HWB2HSV');
+const Colour = require('../../Colour');
+const colours = require('./colours');
+
+describe("HWB2HSV()", () => {
+
+    // Positive test
+    colours.forEach(colour => {
+        it(`should convert ${colour.hwb} to ${colour.hsv}` , () => {
+            const convertFrom = new Colour(colour.hwb).getChannels();
+            const convertTo = new Colour(colour.hsv).getChannels();
+            expect(HWB2HSV(...convertFrom)).toStrictEqual(convertTo);
+        });
+    });
+});
